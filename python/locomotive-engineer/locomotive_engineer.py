fc117073dc9e613964d534c4dde263c932e1a01a
Jakub Porebski <kubusb@gmail.com>
2023-07-14 15:18:24 +0200

    First function completed

--- python/locomotive-engineer/locomotive_engineer.py
+++ python/locomotive-engineer/locomotive_engineer.py
@@ -1,13 +1,14 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*args):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
+
+    return list(args)
 
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
