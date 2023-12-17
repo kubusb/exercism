8212e059fcbf7a22223c7362c17313f45284393c
Jakub Porebski <kubusb@gmail.com>
2023-12-17 17:44:24 +0100

    Collatz Conjecture completed

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,19 @@
+"""
+Collatz Conjecture
+"""
 def steps(number):
-    pass
+    """
+    This function terurns number of steps needed to complete Collatz Conjecture
+    """
+    iteration = 0
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    else:
+        while number > 1:
+            if number % 2 == 0:
+                number /= 2
+                iteration += 1
+            else:
+                number = 3 * number + 1
+                iteration += 1
+        return iteration
