c88a6200430ab662229c04b889ea41e7be84280b
Jakub Porebski <kubusb@gmail.com>
2023-12-17 12:05:25 +0100

    Armstrong numbers completed

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,8 @@
 def is_armstrong_number(number):
-    pass
+    result = 0
+    for digit in str(number):
+        ingreedient = int(digit) ** len(str(number))
+        result = result + ingreedient
+    if result == number:
+        return True
+    return False
