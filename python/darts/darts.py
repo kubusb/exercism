4ea44c693200b39f67f1a0ec0e309bad149150a4
Jakub Porebski <kubusb@gmail.com>
2023-12-19 14:58:54 +0100

    Darts completed

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,18 @@
+import math
+
 def score(x, y):
-    pass
+    x = abs(x)
+    y = abs(y)
+
+    distance_from_center_0 = math.sqrt(x ** 2 + y ** 2)
+    if distance_from_center_0 > 10:
+        return 0
+
+    elif 10 >= distance_from_center_0 > 5:
+        return 1
+
+    elif 5 >= distance_from_center_0 > 1:
+        return 5
+
+    elif distance_from_center_0 <= 1:
+        return 10
