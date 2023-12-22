512f0a14b0be032e24d970c3ab30d314d4fccf76
Jakub Porebski <kubusb@gmail.com>
2023-12-22 23:47:45 +0100

    Exercise completed

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,8 @@
 def is_pangram(sentence):
-    pass
+    letters_set = set()
+    for letter in sentence.lower():
+        if letter.isalpha() and not letter.isspace():
+            letters_set.add(letter)
+    if len(letters_set) == 26:
+        return True
+    return False
