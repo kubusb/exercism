3e6e904bcdefee389660ebe6d7fc54201cca4501
Jakub Porebski <kubusb@gmail.com>
2023-12-23 23:28:48 +0100

    Isogram completed

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,10 @@
 def is_isogram(string):
-    pass
+    letters_set = set()
+    string = string.replace("-", "")
+    string = string.replace(" ", "")
+    for letter in string.lower():
+        if letter.isalpha():
+            letters_set.add(letter)
+    if len(letters_set) == len(string):
+        return True
+    return False
