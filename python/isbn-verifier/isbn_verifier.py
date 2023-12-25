b8e16dc7225f78b87c8b384da08610b9ef9ee7ed
Jakub Porebski <kubusb@gmail.com>
2023-12-25 22:38:41 +0100

    ISBN_verifier completed

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,19 @@
 def is_valid(isbn):
-    pass
+    control_sum = 0
+    index = 10
+    isbn = str(isbn.replace("-", ""))
+    if len(isbn) == 10:
+        if isbn[0:8].isdigit() and (isbn[9].isdigit() or isbn[9] == "X"):
+            if isbn[9].isdigit():
+                control_digit = int(isbn[9])
+            elif isbn[9].upper() == "X":
+                control_digit = 10
+            else:
+                return False
+            for digit in isbn[0:9]:
+                control_sum = control_sum + index * int(digit)
+                index -= 1
+            control_sum = control_sum + int(control_digit)
+            return bool(control_sum % 11 == 0)
+        return False
+    return False
