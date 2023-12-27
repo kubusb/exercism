6be24bc693d86440f584401daf3101322cd29b56
Jakub Porebski <kubusb@gmail.com>
2023-12-27 02:11:06 +0100

    Rotational cypher completed

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,18 @@
 def rotate(text, key):
-    pass
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    encoded_string = ""
+    for letter in text:
+        position = alphabet.find(letter.lower())
+        if position + 1 + key < len(alphabet):
+            new_letter = alphabet[position + key]
+        elif position + 1 + key >= len(alphabet):
+            new_letter = alphabet[position + key - len(alphabet)]
+        if letter.islower():
+            encoded_string += new_letter
+        elif letter.isupper():
+            encoded_string += new_letter.upper()
+        elif letter.isspace():
+            encoded_string += " "
+        else:
+            encoded_string += letter
+    return encoded_string
