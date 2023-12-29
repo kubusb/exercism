01c2f308155b45a96d0691759c4361a355bcb556
Jakub Porebski <kubusb@gmail.com>
2023-12-29 20:42:30 +0100

    RNA Transcription completed

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,14 @@
 def to_rna(dna_strand):
-    pass
+    rna_strand = ""
+    for letter in dna_strand:
+        if letter == "":
+            rna_strand += ""
+        if letter == "G":
+            rna_strand += "C"
+        if letter == "C":
+            rna_strand += "G"
+        if letter == "T":
+            rna_strand += "A"
+        if letter == "A":
+            rna_strand += "U"
+    return rna_strand
