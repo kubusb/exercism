0ddb25f80d2a607eb1dd6d101f2ce1d8e4cb80e7
Jakub Porebski <kubusb@gmail.com>
2024-01-02 21:25:18 +0100

    Wordy completed

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,27 +1,14 @@
-import re
-
-def answer(question):
-    what_is = re.findall(r"^What is (-?[0-9]\d*)\?", question)
-    addition = re.findall(r"^What is (-?[0-9]\d*) plus (-?[0-9]\d*)\?", question)
-    subtraction = re.findall(r"^What is (-?[0-9]\d*) minus (-?[0-9]\d*)\?", question)
-    multiplication = re.findall(r"^What is (-?[0-9]\d*) multiplied by (-?[0-9]\d*)\?", question)
-    division = re.findall(r"^What is (-?[0-9]\d*) divided by (-?[0-9]\d*)\?", question)
-    question = question.replace("What is ", "")
-    question = question.replace("?", "")
-    question = question.replace(" ", "")
-    print(question)
-    if question == "":
-        raise ValueError("syntax error")
-    if len(what_is) > 0:
-        return int(what_is[0])
-    elif len(addition) > 0:
-        return int(addition[0][0]) + int(addition[0][1])
-    elif len(subtraction) > 0:
-        return int(subtraction[0][0]) - int(subtraction[0][1])
-    elif len(multiplication) > 0:
-        return int(multiplication[0][0]) * int(multiplication[0][1])
-    elif len(division) > 0:
-        return int(division[0][0]) / int(division[0][1])
-    else:
+def answer(question: str) -> int:
+    question = question.replace("What is ", "").replace(
+        "?", "").replace("plus", "+").replace("minus", "-").replace("divided by", "/").replace("multiplied by", "*").split(" ")
+    question.insert(0, "(")
+    question.insert(4, ")")
+    
+    q = [x.isalpha() for x in question if x not in ("What", "is")] 
+    if any(q):
         raise ValueError("unknown operation")
-    return False
+    
+    try:
+        return eval(" ".join(question))
+    except:
+        raise ValueError("syntax error")
