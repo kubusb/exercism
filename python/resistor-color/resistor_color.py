10de89b69b893a87f035f5466c955942c5e79847
Jakub Porebski <kubusb@gmail.com>
2024-01-03 12:11:56 +0100

    Simplified

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,33 +1,21 @@
-def color_code(color):
-    resistors = {
-        'black' : 0,
-        'brown' : 1,
-        'red'   : 2,
-        'orange': 3,
-        'yellow': 4,
-        'green' : 5,
-        'blue'  : 6,
-        'violet': 7,
-        'grey'  : 8,
-        'white' : 9
-    }
-    return resistors[color]
+resistors = {
+    'black' : 0,
+    'brown' : 1,
+    'red'   : 2,
+    'orange': 3,
+    'yellow': 4,
+    'green' : 5,
+    'blue'  : 6,
+    'violet': 7,
+    'grey'  : 8,
+    'white' : 9
+}
 
+def color_code(color):
+    return resistors[color]
 
 def colors():
     result = []
-    resistors = {
-        'black' : 0,
-        'brown' : 1,
-        'red'   : 2,
-        'orange': 3,
-        'yellow': 4,
-        'green' : 5,
-        'blue'  : 6,
-        'violet': 7,
-        'grey'  : 8,
-        'white' : 9
-    }
 
     for key in resistors:
         result.append(key)
