decdb564c6ce7684dcc3d736fcb4d29bb54e615a
Jakub Porebski <kubusb@gmail.com>
2024-01-05 11:40:28 +0100

    Resistors color duo completed

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,21 @@
+resistors = {
+    'black' : 0,
+    'brown' : 1,
+    'red'   : 2,
+    'orange': 3,
+    'yellow': 4,
+    'green' : 5,
+    'blue'  : 6,
+    'violet': 7,
+    'grey'  : 8,
+    'white' : 9
+}
+
 def value(colors):
-    pass
+    result = ""
+    number_of_colors = 1
+    for color in colors:
+        if number_of_colors < 3:
+            result += str(resistors[color])
+            number_of_colors += 1
+    return int(result)
