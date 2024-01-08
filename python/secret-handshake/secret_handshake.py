945f1d41d88a0769064b9f01a0983a9416b24bad
Jakub Porebski <kubusb@gmail.com>
2024-01-09 00:21:42 +0100

    Anagram started

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,21 @@
+# 00001 = wink
+# 00010 = double blink
+# 00100 = close your eyes
+# 01000 = jump
+# 10000 = Reverse the order of the operations in the secret handshake.
+
 def commands(binary_str):
-    pass
+    result = []
+    if binary_str == "00000":
+        return result
+    if binary_str[-1] == "1":
+        result.append("wink")
+    if binary_str[-2] == "1":
+        result.append("double blink")
+    if binary_str[-3] == "1":
+        result.append("close your eyes")
+    if binary_str[-4] == "1":
+        result.append("jump")
+    if binary_str[-5] == "1":
+        return result[::-1]
+    return result
