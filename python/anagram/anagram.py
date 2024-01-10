865dcea80be0e598a8a84186833640b7fd59967d
Jakub Porebski <kubusb@gmail.com>
2024-01-10 11:57:18 +0100

    Anagrams started

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,8 @@
 def find_anagrams(word, candidates):
-    pass
+    anagrams = []
+    word_set = set(word)
+    for candidate in candidates:
+        if str(candidate).lower() != str(word).lower():
+            if set(candidate).issubset(word_set):
+                anagrams.append(candidate)
+    return anagrams
