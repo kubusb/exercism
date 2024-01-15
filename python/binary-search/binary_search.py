96edec3706ddf1d56845f854bde98aa83905dc6e
Jakub Porebski <kubusb@gmail.com>
2024-01-15 20:56:22 +0100

    Binary search corrected

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,18 +1,12 @@
-from icecream import ic
-
 def find(search_list, value):
     sorted_search_list = sorted(search_list)
-    ic(sorted_search_list)
-    ic(value)
     if len(sorted_search_list) == 0:
         raise ValueError("value not in array")
     if len(sorted_search_list) == 1:
-        ic(sorted_search_list)
         if sorted_search_list[0] == value:
             return search_list.index(value)
         raise ValueError("value not in array")
     if len(sorted_search_list) % 2 != 0 and len(sorted_search_list) > 1:
-        ic("ODD")
         middle_element = int(len(sorted_search_list) // 2 )
         if sorted_search_list[middle_element] == value:
             return search_list.index(value)
@@ -23,7 +17,6 @@ def find(search_list, value):
             sorted_search_list = sorted_search_list[middle_element:]
             find(sorted_search_list, value)
     elif len(sorted_search_list) % 2 == 0 and len(sorted_search_list) > 1:
-        ic("EVEN")
         middle_element = int(len(sorted_search_list) // 2 )
         if sorted_search_list[middle_element] > value:
             sorted_search_list = sorted_search_list[:middle_element]
