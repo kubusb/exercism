42be46709013834c8805dd85dd50ff2d0bb8b50b
Jakub Porebski <kubusb@gmail.com>
2024-01-16 09:33:35 +0100

    Hamming completed

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,12 @@
 def distance(strand_a, strand_b):
-    pass
+    hamming = 0
+    position = 0
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+    if len(strand_a) == 0:
+        return 0
+    while position < len(strand_a):
+        if strand_a[position] != strand_b[position]:
+            hamming += 1
+        position += 1
+    return hamming
