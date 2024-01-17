770007e62b29036708a35a4a253165c184102881
Jakub Porebski <kubusb@gmail.com>
2024-01-17 13:06:16 +0100

    Array flattener completed

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,12 @@
 def flatten(iterable):
-    pass
+    flattened_list = []
+
+    for item in iterable:
+        if item is None:
+            continue
+        if isinstance(item, list):
+            flattened_list.extend(flatten(item))
+        else:
+            flattened_list.append(item)
+
+    return flattened_list
