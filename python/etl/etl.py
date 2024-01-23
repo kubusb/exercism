c079ac48580e76b2ccd8acf152d93dc8d5490504
Jakub Porebski <kubusb@gmail.com>
2024-01-23 22:35:52 +0100

    ETL completed

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,3 +1,35 @@
+new_points = {
+    "a":1,
+    "b":3,
+    "c":3,
+    "d":2,
+    "e":1,
+    "f":4,
+    "g":2,
+    "h":4,
+    "i":1,
+    "j":8,
+    "k":5,
+    "l":1,
+    "m":3,
+    "n":1,
+    "o":1,
+    "p":3,
+    "q":10,
+    "r":1,
+    "s":1,
+    "t":1,
+    "u":1,
+    "v":4,
+    "w":4,
+    "x":8,
+    "y":4,
+    "z":10
+}
+
 def transform(legacy_data):
-    print(legacy_data)
-    pass
+    result = {}
+    for points in legacy_data:
+        for letters in legacy_data[points]:
+            result.update({letters.casefold(): new_points[letters.casefold()]})
+    return result
