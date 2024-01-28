1664e6ba1f2f7cca7f89a8d87106d082b80263e9
Jakub Porebski <kubusb@gmail.com>
2024-01-29 00:42:24 +0100

    Sum of multiples completed

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,13 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    resulting_set = set()
+    if multiples is None or len(multiples) == 0 or multiples == 0:
+        return 0
+    else:
+        for multiple in multiples:
+            if multiple == 0:
+                continue
+            current = multiple
+            while current < limit:
+                resulting_set.add(current)
+                current = current + multiple
+    return sum(resulting_set)
