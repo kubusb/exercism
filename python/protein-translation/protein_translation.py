5c686395e13fce387ab1314fa719524a2b4138d4
Jakub Porebski <kubusb@gmail.com>
2024-02-10 15:47:02 +0100

    RNA translator completed

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,35 @@
+PROTEIN_MATRIX = {
+    "Codon" :   "Protein",
+    "AUG"   :	"Methionine",
+    "UUU"   :   "Phenylalanine",
+    "UUC"	:   "Phenylalanine",
+    "UUA"   :   "Leucine",
+    "UUG"	:   "Leucine",
+    "UCU"   :   "Serine",
+    "UCC"   :   "Serine",
+    "UCA"   :   "Serine",
+    "UCG"   :	"Serine",
+    "UAU"   :   "Tyrosine",
+    "UAC"   :	"Tyrosine",
+    "UGU"   :   "Cysteine",
+    "UGC"	:   "Cysteine",
+    "UGG"   :   "Tryptophan",
+    "UAA"   :   "STOP",
+    "UAG"   :   "STOP",
+    "UGA"   :   "STOP"
+    }
+
 def proteins(strand):
-    pass
+    result = []
+    split_strands = []
+    current_protein_code = ""
+    for index, letter in enumerate(strand):
+        current_protein_code += letter
+        if (index + 1) % 3 == 0:
+            if current_protein_code == "UAA" or current_protein_code == "UAG" or current_protein_code == "UGA":
+                break
+            split_strands.append(current_protein_code)
+            result.append(PROTEIN_MATRIX[current_protein_code])
+            current_protein_code = ""
+    return result
+            
