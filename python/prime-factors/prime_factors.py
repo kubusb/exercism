b6efd2d0c074576ce38f3420530ad5dc1e316c60
Jakub Porebski <kubusb@gmail.com>
2024-02-11 00:27:06 +0100

    Prime factors completed

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,10 @@
 def factors(value):
-    pass
+    found_divisors = []
+    divisor = 2
+    while value > 1:
+        if value % divisor == 0:
+            found_divisors.append(divisor)
+            value = value / divisor
+        else:
+            divisor += 1
+    return found_divisors
