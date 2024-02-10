df40657716e853a750a1e6645d7a6367ccb83134
Jakub Porebski <kubusb@gmail.com>
2024-02-10 10:21:12 +0100

    Diamond in progress

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -1,3 +1,18 @@
 ALPHABET="ABCDEFGHIJKLMNOPQRSTUVXYZ"
 def rows(letter):
-    pass
+    letter_position = ALPHABET.find(letter)
+    result = []
+    external_spaces = 0
+    internal_spaces = letter_position + 1
+    if letter == "A":
+        result.append("A")
+    else:
+        while internal_spaces > 0:
+            print("{}{}{}{}{}".format(external_spaces, ALPHABET[letter_position], internal_spaces, ALPHABET[letter_position], external_spaces))
+            external_spaces += 1
+            internal_spaces -= 2
+            letter_position -= 1
+    return result
+#-A-
+#B--B
+#-A-
