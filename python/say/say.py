5b23104a70de5b63c1b51eb6f0a2ca9e1122c2ca
Jakub Porebski <kubusb@gmail.com>
2024-02-22 14:18:17 +0100

    Continuation of say

--- python/say/say.py
+++ python/say/say.py
@@ -1,3 +1,32 @@
+simple_digit_mapping = {
+    0:"zero"
+    1:"one"
+    2:"two"
+    3:"three"
+    4:"four"
+    5:"five"
+    6:"six"
+    7:"seven"
+    8:"eight"
+    9:"nine"
+}
+
 def say(number):
-    if number < 1 or number > 999_999_999_999:
+    number_list = []
+    current_split = ""
+    if number < 0 or number > 999_999_999_999:
         raise ValueError("input out of range")
+
+    for index, digit in enumerate(str(number)):
+        if len(str(number)) <= 3:
+            number_list.append(number)
+            break
+        else:
+            print("Number was: {}, I:{}, D:{}".format(number, index, digit))
+    print("Number is shorter than 4!")
+    print(number_list)
+    #     current_split += digit
+    #     if (index + 1) % 3 == 0:
+    #         number_list.appnd(current_split)
+    #         current_split = ""
+    # print(number_list)
