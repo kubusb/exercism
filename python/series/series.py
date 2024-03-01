7f0c9cd4c36b6e5cc8f848c5c90c0655ef862ddb
Jakub Porebski <kubusb@gmail.com>
2024-03-01 10:13:34 +0100

    Error handling for series completed

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,19 @@
+""" Series """
 def slices(series, length):
-    pass
+    """ Series """
+    # Error handling
+    if length == 0:
+        # if the slice length is zero.
+        raise ValueError("slice length cannot be zero")
+
+    if length < 0:
+        # if the slice length is negative.
+        raise ValueError("slice length cannot be negative")
+
+    if series == "" or series is None:
+        # if the series provided is empty.
+        raise ValueError("series cannot be empty")
+
+    if len(series) < length:
+        # if the slice length is longer than the series.
+        raise ValueError("slice length cannot be greater than series length")
