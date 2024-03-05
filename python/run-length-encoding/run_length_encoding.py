947f43e4875412b68b29e5047700dfd9ae8393e7
Jakub Porebski <kubusb@gmail.com>
2024-03-05 12:10:21 +0100

    Encoding  all different letters completed

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -2,7 +2,16 @@ def decode(string):
     if string == "":
         return ""
 
-
 def encode(string):
     if string == "":
         return ""
+    
+    current_position = 0
+    result = ""
+    while current_position <= len(string):
+        if string[current_position:current_position+1] != string[current_position+1:current_position+2]:
+            result += string[current_position:current_position+1]
+            current_position += 1
+        else:
+            break
+    return result
