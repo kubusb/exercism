91beb34f6822abf2222a31265283f33ac08386b1
Jakub Porebski <kubusb@gmail.com>
2024-03-07 12:19:59 +0100

    Decode function completed and working

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,15 @@
-def decode(string):
-    if string == "":
-        return ""
+def decode(encoded_str):
+    decoded_str = ""
+    i = 0
+    while i < len(encoded_str):
+        count_str = ''
+        while encoded_str[i].isdigit():
+            count_str += encoded_str[i]
+            i += 1
+        count = int(count_str) if count_str else 1
+        decoded_str += encoded_str[i] * count
+        i += 1
+    return decoded_str
 
 def encode(input_str):
     if not input_str:
