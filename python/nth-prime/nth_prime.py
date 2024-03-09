3ad42f45a85c65817200f470f65c2489ddce158d
Jakub Porebski <kubusb@gmail.com>
2024-03-09 10:55:09 +0100

    Case for first pn added

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,7 +1,11 @@
 def prime(number):
-    pass
+    current_number = 2
+    found_prime = 1
 
-# Start: Zacznij od liczby 2 (pierwsza liczba pierwsza) i ustaw licznik znalezionych liczb pierwszych na 1.
+    while found_prime <= number:
+        if found_prime == number:
+            return current_number
+        break
 # Sprawdzanie liczby: Dla każdej liczby sprawdź, czy jest liczbą pierwszą. Można to zrobić, dzieląc ją przez wszystkie liczby mniejsze od niej i większe od 1. Jeśli żaden dzielnik nie dzieli jej bez reszty, liczba jest pierwsza.
 # Licznik: Jeśli liczba jest pierwsza, zwiększ licznik liczb pierwszych o 1. Jeśli licznik równa się n, znaleziono n-tą liczbę pierwszą.
 # Powtórzenie: Jeśli nie osiągnięto jeszcze n-tej liczby pierwszej, przejdź do następnej liczby i powtórz proces.
