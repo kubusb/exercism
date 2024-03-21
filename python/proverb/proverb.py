13ef62a6e29dd23d3d0f9fbfd28300e405a326dd
Jakub Porebski <kubusb@gmail.com>
2024-03-21 18:42:55 +0100

    Proverb completed

--- python/proverb/proverb.py
+++ python/proverb/proverb.py
@@ -1,2 +1,17 @@
-def proverb():
-    pass
+def proverb(*inputs, qualifier):
+    if not inputs:
+        return []
+
+    proverb = []
+    for i in range(len(inputs) - 1):
+        line = f"For want of a {inputs[i]} the {inputs[i + 1]} was lost."
+        proverb.append(line)
+    
+    if qualifier is None:
+        last_line = f"And all for the want of a {inputs[0]}."  # Initialize the last line
+        proverb.append(last_line)
+    else:
+        last_line = f"And all for the want of a {qualifier} {inputs[0]}."  # Initialize the last line
+        proverb.append(last_line)       
+
+    return proverb
