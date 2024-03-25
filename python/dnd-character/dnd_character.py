c6906afccdbaacf2484c834fe40851a62fd632b9
Jakub Porebski <kubusb@gmail.com>
2024-03-25 19:09:00 +0100

    DnD Char needs reworking

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,3 +1,24 @@
+import random
+from math import floor
+
 class Character:
     def __init__(self):
-        pass
+        self.strength = self.roll_ability()
+        self.dexterity = self.roll_ability()
+        self.constitution = self.roll_ability()
+        self.intelligence = self.roll_ability()
+        self.wisdom = self.roll_ability()
+        self.charisma = self.roll_ability()
+        self.hitpoints = self.calculate_hitpoints()
+
+    def roll_ability(self):
+        dice = [random.randint(1, 6) for _ in range(4)]
+        return sum(sorted(dice)[1:])
+
+    def calculate_hitpoints(self):
+        constitution_modifier = floor((self.constitution - 10) / 2)
+        return 10 + constitution_modifier
+
+def modifier(self):
+    constitution_modifier = floor((Character.constitution - 10) / 2)
+    return 10 + constitution_modifier
