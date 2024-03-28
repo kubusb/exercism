ad325df24da2fd4f98f6d2ad16cac62c95578009
Jakub Porebski <kubusb@gmail.com>
2024-03-28 20:16:13 +0100

    Robots can say hi

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,18 @@
+import random
+import string
+
 class Robot:
+    existing_names = set()
+
     def __init__(self):
-        pass
+        self.name = self.generate_name()
+
+    def generate_name(self):
+        while True:
+            name = ''.join(random.choices(string.ascii_uppercase, k=2)) + ''.join(random.choices(string.digits, k=3))
+            if name not in self.existing_names:
+                self.existing_names.add(name)
+                return name
+
+    def reset(self):
+        self.name = self.generate_name()
