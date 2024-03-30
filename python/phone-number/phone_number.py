7e52eb8ffd3971934ed2c3f6cfc3be0a55fb30a9
Jakub Porebski <kubusb@gmail.com>
2024-03-30 09:20:16 +0100

    Let's start again - this time with exceptions

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,3 +1,11 @@
 class PhoneNumber:
     def __init__(self, number):
-        pass
+        # Remove all non-digit characters
+        cleaned_number = ''.join(filter(str.isdigit, number))
+        
+        # Set the cleaned number as the instance attribute
+        self.number = cleaned_number
+        
+    def __str__(self):
+        # Format the phone number as XXXXXXXXXX
+        return self.number
