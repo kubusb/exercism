36046a20ed71428eddab9e683039505102c0e4d8
Jakub Porebski <kubusb@gmail.com>
2024-04-21 07:54:00 +0200

    High scores completed

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,3 +1,15 @@
 class HighScores:
     def __init__(self, scores):
-        pass
+        self.scores = scores
+
+    def latest(self):
+        """Return the most recently added score."""
+        return self.scores[-1]
+
+    def personal_best(self):
+        """Return the highest score from the list."""
+        return max(self.scores)
+
+    def personal_top_three(self):
+        """Return the top three highest scores sorted from highest to lowest."""
+        return sorted(self.scores, reverse=True)[:3]
