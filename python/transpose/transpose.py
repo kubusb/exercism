fe0037abc91fb3216caec3e474e09d6c902e8892
Jakub Porebski <kubusb@gmail.com>
2024-04-27 10:49:26 +0200

    Single line case completed

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,3 +1,9 @@
 def transpose(lines):
+    # If lines are empty, return nothing.
     if len(lines) == 0:
         return ""
+    
+    # If there is only one line, transposition will be easy:
+    if not "\n" in lines:
+        newline = "\n"
+        return newline.join(list(lines))
