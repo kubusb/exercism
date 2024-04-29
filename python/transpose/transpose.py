0405de587ebc779dde84586546cc0fb479f140ef
Jakub Porebski <kubusb@gmail.com>
2024-04-29 10:33:52 +0200

    Trnasposition counter is set, we now know which line is the longest

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,13 +1,21 @@
 def transpose(lines):
+    newline = "\n"
     # If lines are empty, return nothing.
     if len(lines) == 0:
         return ""
     
     # If there is only one line, transposition will be easy:
     if not "\n" in lines:
-        newline = "\n"
         return newline.join(list(lines))
     
     # Count number of symbols in each line, decide which line is the longest.
     # Edit if lines are too short - add spaces to the END of the lines.
     # Then transpose 1:1 keeping the spaces.
+    counter_longest_line_symbol_count = 0
+    list_of_lines = lines.split(newline)
+    #print(list_of_lines)
+    for line in list_of_lines:
+        # print("Line:[{}], length:{}".format(line, len(line)))
+        if len(line) > counter_longest_line_symbol_count:
+            counter_longest_line_symbol_count = len(line)
+    print("Longest line detected has the following number of chars: {}".format(counter_longest_line_symbol_count))
