2ef6834fdb040aee132b1729a5ef44792c2d37f4
Jakub Porebski <kubusb@gmail.com>
2024-05-03 14:02:25 +0200

    Transpose completed

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -15,16 +15,15 @@ def transpose(lines):
     for line in list_of_lines:
         if len(line) > counter_longest_line_symbol_count:
             counter_longest_line_symbol_count = len(line)
-    print("Longest line detected has the following number of chars: {}".format(counter_longest_line_symbol_count))
+    #print("Longest line detected has the following number of chars: {}".format(counter_longest_line_symbol_count))
 
     # Edit if lines are too short - add spaces to the END of the lines.
     squared_list_of_lines = []
     for line in list_of_lines:
-        if len(line) < counter_longest_line_symbol_count:
+        if len(line) <= counter_longest_line_symbol_count:
             line = line + (counter_longest_line_symbol_count - len(line))*" "
         squared_list_of_lines.append(line)
 
-    
     # Then transpose 1:1 keeping the spaces.
     # line_counter = 0
     # line_position = 0
@@ -37,9 +36,18 @@ def transpose(lines):
     #         line_position += 1
     #     result.append(transposed_line)
     # print(result)
-    print(squared_list_of_lines)
+    #print(squared_list_of_lines)
     #print("TyleLinii:-{}-,\nPoTyleZnakow:-{}-".format(len(list_of_lines), counter_longest_line_symbol_count))
-    print("WynikTyleLinii:={}=\nPoTyleZnakow={}=".format(counter_longest_line_symbol_count, len(list_of_lines)))
+    #print("WynikTyleLinii:={}=\nPoTyleZnakow={}=".format(counter_longest_line_symbol_count, len(squared_list_of_lines)))
     
-    counter = 0
-    transposed_line = ""
+    result = []
+    liczba_starych_linii = len(squared_list_of_lines)
+    liczba_starych_znakow = counter_longest_line_symbol_count
+
+    for liczba_nowych_linii in range(liczba_starych_znakow):
+        nowa_linia = ""
+        for liczba_nowych_znakow in range(liczba_starych_linii):
+            nowa_linia = nowa_linia + squared_list_of_lines[liczba_nowych_znakow][liczba_nowych_linii]
+        result.append(nowa_linia)
+        print("|{}|".format(nowa_linia))
+    return newline.join(list(result)).rstrip()
