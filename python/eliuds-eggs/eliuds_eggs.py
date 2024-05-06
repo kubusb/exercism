6729457a96ad43fd463dd6272a12b0b27d5d98cb
Jakub Porebski <kubusb@gmail.com>
2024-05-06 15:14:05 +0200

    Egg counting completed

--- python/eliuds-eggs/eliuds_eggs.py
+++ python/eliuds-eggs/eliuds_eggs.py
@@ -1,3 +1,9 @@
 def egg_count(display_value):
     # Let's start by converting decimal to binary:
-    
+    quotient = display_value
+    binary_result = []
+    while quotient > 0:
+        remainder = int(quotient % 2)
+        quotient = int(quotient / 2)
+        binary_result.append(remainder)
+    return binary_result.count(1)
