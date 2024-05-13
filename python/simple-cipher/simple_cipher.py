2a29990da93d1f797557effec7cafce58bc0f0f8
Jakub Porebski <kubusb@gmail.com>
2024-05-13 09:13:49 +0200

    Cipher completed

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,9 +1,23 @@
+import random
+import string
+
 class Cipher:
     def __init__(self, key=None):
-        pass
-
+        if key is None:
+            self.key = ''.join(random.choices(string.ascii_lowercase, k=100))
+        elif all(char in string.ascii_lowercase for char in key):
+            self.key = key
+        else:
+            raise ValueError("Key must only contain lowercase letters.")
+    
     def encode(self, text):
-        pass
+        return ''.join(
+            chr(((ord(char) - ord('a') + ord(self.key[i % len(self.key)]) - ord('a')) % 26) + ord('a'))
+            for i, char in enumerate(text)
+        )
 
     def decode(self, text):
-        pass
+        return ''.join(
+            chr(((ord(char) - ord('a') - (ord(self.key[i % len(self.key)]) - ord('a'))) % 26) + ord('a'))
+            for i, char in enumerate(text)
+        )
