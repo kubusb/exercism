282c5a91887eba8b23db58bb23342ab52f534050
Jakub Porebski <kubusb@gmail.com>
2024-05-25 11:43:23 +0200

    Clock implemented with __retr__

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,18 +1,20 @@
 class Clock:
     def __init__(self, hour, minute):
-        pass
+        self.total_minutes = (hour * 60 + minute) % (24 * 60)
 
     def __repr__(self):
-        pass
+        hours, minutes = divmod(self.total_minutes, 60)
+        return f"Clock({hours}, {minutes})"
 
     def __str__(self):
-        pass
+        hours, minutes = divmod(self.total_minutes, 60)
+        return f"{hours:02}:{minutes:02}"
 
     def __eq__(self, other):
-        pass
+        return self.total_minutes == other.total_minutes
 
     def __add__(self, minutes):
-        pass
+        return Clock(0, self.total_minutes + minutes)
 
     def __sub__(self, minutes):
-        pass
+        return Clock(0, self.total_minutes - minutes)
