ade4504460737fc79f01b2dea529e075aa8827e3
Jakub Porebski <kubusb@gmail.com>
2024-06-27 18:11:02 +0200

    Simple linked list completed

--- python/simple-linked-list/simple_linked_list.py
+++ python/simple-linked-list/simple_linked_list.py
@@ -1,33 +1,59 @@
+class EmptyListException(Exception):
+    """Exception raised when the linked list is empty."""
+    pass
+
+
 class Node:
     def __init__(self, value):
-        pass
+        self._value = value
+        self._next = None
 
     def value(self):
-        pass
+        return self._value
 
     def next(self):
-        pass
+        return self._next
 
 
 class LinkedList:
     def __init__(self, values=[]):
-        pass
+        self.head_node = None
+        self.size = 0
+        for value in values:
+            self.push(value)
 
     def __len__(self):
-        pass
+        return self.size
+
+    def __iter__(self):
+        current = self.head_node
+        while current:
+            yield current.value()
+            current = current.next()
 
     def head(self):
-        pass
+        if not self.head_node:
+            raise EmptyListException("The list is empty.")
+        return self.head_node
 
     def push(self, value):
-        pass
+        new_node = Node(value)
+        new_node._next = self.head_node
+        self.head_node = new_node
+        self.size += 1
 
     def pop(self):
-        pass
+        if not self.head_node:
+            raise EmptyListException("The list is empty.")
+        value = self.head_node.value()
+        self.head_node = self.head_node.next()
+        self.size -= 1
+        return value
 
     def reversed(self):
-        pass
-
-
-class EmptyListException(Exception):
-    pass
+        new_list = LinkedList()
+        current = self.head_node
+        while current:
+            new_list.push(current.value())
+            current = current.next()
+        return new_list
