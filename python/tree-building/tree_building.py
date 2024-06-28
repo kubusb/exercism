1447081164f36be0f00a5fe7fde9a677902d7d8b
Jakub Porebski <kubusb@gmail.com>
2024-06-28 09:33:16 +0200

    Tree build refactoring completed

--- python/tree-building/tree_building.py
+++ python/tree-building/tree_building.py
@@ -11,40 +11,37 @@ class Node:
 
 
 def BuildTree(records):
-    root = None
+    if not records:
+        return None
+
     records.sort(key=lambda x: x.record_id)
-    ordered_id = [i.record_id for i in records]
-    if records:
-        if ordered_id[-1] != len(ordered_id) - 1:
-            raise ValueError('broken tree')
-        if ordered_id[0] != 0:
-            raise ValueError('invalid')
-    trees = []
-    parent = {}
-    for i in range(len(ordered_id)):
-        for j in records:
-            if ordered_id[i] == j.record_id:
-                if j.record_id == 0:
-                    if j.parent_id != 0:
-                        raise ValueError('error!')
-                if j.record_id < j.parent_id:
-                    raise ValueError('something went wrong!')
-                if j.record_id == j.parent_id:
-                    if j.record_id != 0:
-                        raise ValueError('error!')
-                trees.append(Node(ordered_id[i]))
-    for i in range(len(ordered_id)):
-        for j in trees:
-            if i == j.node_id:
-                parent = j
-        for j in records:
-            if j.parent_id == i:
-                for k in trees:
-                    if k.node_id == 0:
-                        continue
-                    if j.record_id == k.node_id:
-                        child = k
-                        parent.children.append(child)
-    if len(trees) > 0:
-        root = trees[0]
-    return root
+
+    if records[0].record_id != 0:
+        raise ValueError("Record id is invalid or out of order.")
+
+    record_ids = [record.record_id for record in records]
+    for i in range(len(records)):
+        if i not in record_ids:
+            raise ValueError("Record id is invalid or out of order.")
+
+    nodes = {}
+    for record in records:
+        if record.record_id in nodes:
+            raise ValueError(f"Duplicate record ID {record.record_id} found.")
+        nodes[record.record_id] = Node(record.record_id)
+
+    for record in records:
+        if record.record_id != 0 and record.record_id == record.parent_id:
+            raise ValueError("Only root should have equal record and parent id.")
+        if record.record_id < record.parent_id:
+            raise ValueError("Node parent_id should be smaller than it's record_id.")
+        if record.parent_id not in nodes:
+            raise ValueError("Parent id is invalid or out of order.")
+        if record.record_id not in nodes:
+            raise ValueError("Record id is invalid or out of order.")
+
+        if record.record_id != 0:
+            parent_node = nodes[record.parent_id]
+            parent_node.children.append(nodes[record.record_id])
+
+    return nodes[0]
