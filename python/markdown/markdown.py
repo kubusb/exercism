582cf877885b7dcae89f18c94e13cadc269657d8
Jakub Porebski <kubusb@gmail.com>
2024-07-11 12:49:36 +0200

    Markdown completed

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -1,77 +1,50 @@
 import re
 
-
 def parse(markdown):
-    lines = markdown.split('\n')
-    res = ''
-    in_list = False
-    in_list_append = False
-    for i in lines:
-        if re.match('###### (.*)', i) is not None:
-            i = '<h6>' + i[7:] + '</h6>'
-        elif re.match('##### (.*)', i) is not None:
-            i = '<h5>' + i[6:] + '</h5>'
-        elif re.match('#### (.*)', i) is not None:
-            i = '<h4>' + i[5:] + '</h4>'
-        elif re.match('### (.*)', i) is not None:
-            i = '<h3>' + i[4:] + '</h3>'
-        elif re.match('## (.*)', i) is not None:
-            i = '<h2>' + i[3:] + '</h2>'
-        elif re.match('# (.*)', i) is not None:
-            i = '<h1>' + i[2:] + '</h1>'
-        m = re.match(r'\* (.*)', i)
-        if m:
-            if not in_list:
-                in_list = True
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                    is_italic = True
-                i = '<ul><li>' + curr + '</li>'
-            else:
-                is_bold = False
-                is_italic = False
-                curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    is_italic = True
-                if is_bold:
-                    curr = m1.group(1) + '<strong>' + \
-                        m1.group(2) + '</strong>' + m1.group(3)
-                if is_italic:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                        '</em>' + m1.group(3)
-                i = '<li>' + curr + '</li>'
-        else:
-            if in_list:
-                in_list_append = True
-                in_list = False
+    # Define regex patterns
+    header_pattern = r'^(#{1,6}) (.+)'
+    list_item_pattern = r'^\* (.+)'
+    bold_pattern = r'__(.+?)__'
+    italic_pattern = r'_(.+?)_'
 
-        m = re.match('<h|<ul|<p|<li', i)
-        if not m:
-            i = '<p>' + i + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', i)
-        if m:
-            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', i)
-        if m:
-            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
-        if in_list_append:
-            i = '</ul>' + i
-            in_list_append = False
-        res += i
+    # Split markdown into lines
+    lines = markdown.split('\n')
+    html = []
+    in_list = False
+
+    for line in lines:
+        # Process headers
+        if header_match := re.match(header_pattern, line):
+            level, content = header_match.groups()
+            html.append(f'<h{len(level)}>{content}</h{len(level)}>')
+            continue
+
+        # Process list items
+        if list_match := re.match(list_item_pattern, line):
+            if not in_list:
+                html.append('<ul>')
+                in_list = True
+            content = list_match.group(1)
+            html.append(f'<li>{process_inline_markup(content, bold_pattern, italic_pattern)}</li>')
+            continue
+
+        # Close list if necessary
+        if in_list:
+            html.append('</ul>')
+            in_list = False
+
+        # Process paragraph
+        html.append(f'<p>{process_inline_markup(line, bold_pattern, italic_pattern)}</p>')
+
+    # Close any open list
     if in_list:
-        res += '</ul>'
-    return res
+        html.append('</ul>')
+
+    return ''.join(html)
+
+def process_inline_markup(text, bold_pattern, italic_pattern):
+    # Process bold text
+    text = re.sub(bold_pattern, r'<strong>\1</strong>', text)
+    # Process italic text
+    text = re.sub(italic_pattern, r'<em>\1</em>', text)
+    return text
