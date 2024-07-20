54f1434b96923db632e8394d3ed45c1498d283ca
Jakub Porebski <kubusb@gmail.com>
2024-07-20 22:06:57 +0200

    Binary tree search completed

--- python/binary-search-tree/binary_search_tree.py
+++ python/binary-search-tree/binary_search_tree.py
@@ -1,8 +1,8 @@
 class TreeNode:
     def __init__(self, data, left=None, right=None):
-        self.data = None
-        self.left = None
-        self.right = None
+        self.data = data
+        self.left = left
+        self.right = right
 
     def __str__(self):
         return f'TreeNode(data={self.data}, left={self.left}, right={self.right})'
@@ -10,10 +10,38 @@ class TreeNode:
 
 class BinarySearchTree:
     def __init__(self, tree_data):
-        pass
+        self.root = None
+        for data in tree_data:
+            self.insert(data)
+
+    def insert(self, data):
+        if not self.root:
+            self.root = TreeNode(data)
+        else:
+            self._insert_recursive(self.root, data)
+
+    def _insert_recursive(self, node, data):
+        if data <= node.data:
+            if node.left is None:
+                node.left = TreeNode(data)
+            else:
+                self._insert_recursive(node.left, data)
+        else:
+            if node.right is None:
+                node.right = TreeNode(data)
+            else:
+                self._insert_recursive(node.right, data)
 
     def data(self):
-        pass
+        return self.root
 
     def sorted_data(self):
-        pass
+        return self._inorder_traversal(self.root)
+
+    def _inorder_traversal(self, node):
+        result = []
+        if node:
+            result.extend(self._inorder_traversal(node.left))
+            result.append(node.data)
+            result.extend(self._inorder_traversal(node.right))
+        return result
