10bfc8238e8f8fbc142ea015adc24be567db7c56
Jakub Porebski <kubusb@gmail.com>
2024-08-11 10:25:15 +0200

    Bank account completed

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -1,18 +1,45 @@
+import threading
+
 class BankAccount:
     def __init__(self):
-        pass
+        self._balance = 0
+        self._is_open = False
+        self._lock = threading.Lock()
 
     def get_balance(self):
-        pass
+        with self._lock:
+            if not self._is_open:
+                raise ValueError('account not open')
+            return self._balance
 
     def open(self):
-        pass
+        with self._lock:
+            if self._is_open:
+                raise ValueError('account already open')
+            self._is_open = True
+            self._balance = 0
 
     def deposit(self, amount):
-        pass
+        with self._lock:
+            if not self._is_open:
+                raise ValueError('account not open')
+            if amount <= 0:
+                raise ValueError('amount must be greater than 0')
+            self._balance += amount
 
     def withdraw(self, amount):
-        pass
+        with self._lock:
+            if not self._is_open:
+                raise ValueError('account not open')
+            if amount <= 0:
+                raise ValueError('amount must be greater than 0')
+            if amount > self._balance:
+                raise ValueError('amount must be less than balance')
+            self._balance -= amount
 
     def close(self):
-        pass
+        with self._lock:
+            if not self._is_open:
+                raise ValueError('account not open')
+            self._is_open = False
+            self._balance = 0
