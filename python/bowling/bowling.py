ec6fb6035753f5c5af4183b18719faf47c956137
Jakub Porebski <kubusb@gmail.com>
2024-08-28 13:05:35 +0200

    Rest API started

--- python/bowling/bowling.py
+++ python/bowling/bowling.py
@@ -2,6 +2,7 @@ class BowlingGame:
     def __init__(self):
         self.rolls = []
         self.current_frame = 1
+        self.current_frame_score = 0
 
     def roll(self, pins):
         if pins < 0 or pins > 10:
@@ -11,32 +12,37 @@ class BowlingGame:
             raise ValueError("Cannot roll after game is over")
 
         if self.current_frame < 10:
-            if len(self.rolls) % 2 == 1 and self.rolls[-1] + pins > 10:
+            if self.current_frame_score + pins > 10:
                 raise ValueError("Two rolls in a frame cannot score more than 10 points")
-
-        if self.current_frame == 10:
-            if len(self.rolls) == 20 and self.rolls[18] == 10 and self.rolls[19] < 10 and pins == 10:
-                raise ValueError("Second bonus roll cannot be a strike if first one is not")
-            if len(self.rolls) == 20 and self.rolls[18] + self.rolls[19] == 10 and pins > 10:
-                raise ValueError("Bonus roll cannot score more than 10 points")
 
         self.rolls.append(pins)
 
         if self.current_frame < 10:
-            if self._is_strike(len(self.rolls) - 1) or len(self.rolls) % 2 == 0:
+            if pins == 10:  # Strike
                 self.current_frame += 1
+                self.current_frame_score = 0
+            else:
+                self.current_frame_score += pins
+                if len(self.rolls) % 2 == 0:  # End of frame
+                    self.current_frame += 1
+                    self.current_frame_score = 0
         elif self.current_frame == 10:
-            if len(self.rolls) == 20 and not self._is_strike(18) and not self._is_spare(18):
-                self.current_frame += 1
-            elif len(self.rolls) == 21 and (self._is_strike(18) or self._is_spare(18)):
-                self.current_frame += 1
+            if len(self.rolls) == 20:
+                if self.rolls[18] == 10:  # Strike in 10th frame
+                    self.current_frame_score = 0
+                else:
+                    self.current_frame_score = self.rolls[18]
+            elif len(self.rolls) == 21:
+                if self.rolls[18] == 10:  # Strike in 10th frame
+                    self.current_frame_score = self.rolls[19]
+                elif self.rolls[18] + self.rolls[19] == 10:  # Spare in 10th frame
+                    self.current_frame_score = 0
+                else:
+                    self.current_frame += 1
             elif len(self.rolls) == 22:
                 self.current_frame += 1
 
     def score(self):
-        if not self.is_game_complete():
-            raise ValueError("Game is not complete, cannot be scored")
-
         score = 0
         roll_index = 0
 
@@ -48,7 +54,7 @@ class BowlingGame:
                 score += 10 + self._spare_bonus(roll_index)
                 roll_index += 2
             else:
-                score += self.rolls[roll_index] + self.rolls[roll_index + 1]
+                score += sum(self.rolls[roll_index:roll_index+2])
                 roll_index += 2
 
         return score
@@ -57,23 +63,21 @@ class BowlingGame:
         return roll_index < len(self.rolls) and self.rolls[roll_index] == 10
 
     def _is_spare(self, roll_index):
-        return roll_index + 1 < len(self.rolls) and self.rolls[roll_index] + self.rolls[roll_index + 1] == 10
+        return roll_index + 1 < len(self.rolls) and sum(self.rolls[roll_index:roll_index+2]) == 10
 
     def _strike_bonus(self, roll_index):
-        return sum(self.rolls[roll_index + 1 : roll_index + 3])
+        bonus = sum(self.rolls[roll_index+1:roll_index+3])
+        return bonus if roll_index + 2 < len(self.rolls) else 0
 
     def _spare_bonus(self, roll_index):
-        return self.rolls[roll_index + 2]
+        return self.rolls[roll_index + 2] if roll_index + 2 < len(self.rolls) else 0
 
     def is_game_over(self):
-        return self.current_frame > 10
-
-    def is_game_complete(self):
         if len(self.rolls) < 20:
             return False
-        if len(self.rolls) == 20 and not self._is_strike(18) and not self._is_spare(18):
+        if len(self.rolls) == 20 and self.rolls[18] + self.rolls[19] < 10:
             return True
-        if len(self.rolls) == 21 and not self._is_strike(18):
+        if len(self.rolls) == 21 and self.rolls[18] != 10:
             return True
         if len(self.rolls) == 22:
             return True
